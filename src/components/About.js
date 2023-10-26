19366001d17fe7a09b6c20dcfed465f07f5e0920
kaloa2025 <aalokchoudhari2021@gmail.com>
2023-10-26 09:34:29 +0530

    AalokChoudhari

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,12 @@ function About() {
   return (
     <div>
         <h1 className={styles.primary}>About Page</h1>
-        <h2 className={styles.fontxl}>About Us</h2>
+        {/* <h2 className={styles.fontxl}>About Us</h2>
         <h3 className={`${styles.primary} ${styles.fontxl}`}>Hello!</h3>
         <h4 className={`${styles.fontxl} ${'primary'}`}>I am Aalok!</h4>
+        <h5 className={`primary ${styles.fontxl}`}>Nice meeting you !</h5>
+        <h5 className={`primary ${styles.primary}`}>Nice meeting you !</h5> */}
+        
     </div>
   )
 }
