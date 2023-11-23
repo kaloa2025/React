d9fa9244a480038c8585be82a32afc3a7ca40a3e
kaloa2025 <aalokchoudhari2021@gmail.com>
2023-11-23 10:04:06 +0530

    Aalok-23/11/2023-10.04

---
+++ nodejs/app1.js
@@ -0,0 +1,53 @@
+// console.log("GoodMorning");
+
+// function greeting(greet){
+//     console.log(greet);
+// }
+// greeting("HelloWorld!");
+
+// var http=require('http');
+// http.createServer(function(req,res){
+//     // res.writeHead(200,{'Content-Type':'text/html'});
+//     // res.write('Good Morning');
+//     // res.end();
+
+//     res.writeHead(200,{
+//         'Content-Type':'text/html'
+//     })
+//     .end('Good Afternoon')
+
+// }).listen(8000);
+
+// const http =require('http')
+// const hostname='192.168.56.1';
+// const port=3000;
+// const server=http.createServer((req,res)=>
+// {
+//     res.statusCode=200;
+//     res.setHeader('Content-Type','text/plain');
+//     res.end('Good Night !');
+// });
+// server.listen(port,hostname,()=>{
+//     console.log('Server started on port '+port);
+// });
+
+const http =require('http')
+const fs=require('fs');
+const hostname='192.168.56.1';
+const port=3000;
+
+fs.readFile('index.html',(err,data)=>{
+    if(err)
+        throw(err);
+
+const server= http.createServer((req,res)=>{
+        res.statusCode=200;
+        res.setHeader('Content-Type','text-html');
+        res.write(data);
+        res.end();
+    });
+server.listen(port,hostname,()=>{
+    console.log('Server started on port '+port);
+});
+});
+
