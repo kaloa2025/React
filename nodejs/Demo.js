3d02415877064991fca08de0f755bfd6f0474388
kaloa2025 <aalokchoudhari2021@gmail.com>
2023-11-24 09:44:28 +0530

    /

--- nodejs/Demo.js
+++ nodejs/Demo.js
@@ -1,2 +1,7 @@
-var http=require('http')
-var dt=require()
+var http=require('http');
+var dt=require('./myfirstmodule');
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write('The date is : '+dt.myDateTime());
+    res.end();
+}).listen(9000);
