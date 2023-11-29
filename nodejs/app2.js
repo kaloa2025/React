1287698818147f74199d721dbe39e51abfdd9a46
kaloa2025 <aalokchoudhari2021@gmail.com>
2023-11-29 09:46:36 +0530

    App3.js

--- nodejs/app2.js
+++ nodejs/app2.js
@@ -2,3 +2,4 @@ var https=require('http');
 https.createServer(function(req,res){
     res.writeHead(301,{Location:'http://www.rknec.edu'}).end();
 }).listen(8001);
+//opening a website : status code : 301
---
+++ nodejs/app3.js
@@ -0,0 +1,25 @@
+//blocking & unblocking 
+
+//#BLOCKING - one job will block others until it finishes
+// const fs=require('fs')
+// const data=fs.readFileSync('abc.txt','utf-8');
+// console.log(data);
+// console.log("Blocking Code"); // will run after console.log
+
+//#Non-Blocking
+// const fs=require('fs')
+// fs.readFile('abc.txt','utf-8',(err,data)=>{
+//     if(err) throw err;
+//     console.log(data);
+// });
+// console.log("Non-Blocking Code");
+//utf-8 : code dikhaenga , hata do content dikhaenga
+// res-> response
+
+//Sync callback
+function greetings(callback)
+{
+    callback();
+}
+greetings(()=>{console.log('Hi');});
+console.log("Sync Callback");
